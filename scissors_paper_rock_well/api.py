89b50762d4f6839b540f37106561921a23054ece
npujol <naivy.luna@gmail.com>
2021-02-27 13:56:27 +0100

    Add endpoint to post a play

--- scissors_paper_rock_well/api.py
+++ scissors_paper_rock_well/api.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from scissors_paper_rock_well.models import Input, Output
+from scissors_paper_rock_well.core import play
 
-app = FastAPI()
+app = FastAPI(
+    title="scissors_paper_rock_well",
+    description='A version of the game scissor-paper-rock, but including a new word "well".',
+    version="0.1.0",
+    docs_url="/",
+)
 
 
 # Create a endpoint to post a play
-def create_play(input: Input) -> Output:
-    pass
+@app.post("/play/", response_model=Output)
+async def create_play(input: Input) -> Output:
+    return play(input)
