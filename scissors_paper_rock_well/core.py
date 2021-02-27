1ca026c0a6b2fb99c2db18ca869f48bc4ebb9af9
npujol <naivy.luna@gmail.com>
2021-02-27 11:43:37 +0100

    Add the core's functions

--- scissors_paper_rock_well/core.py
+++ scissors_paper_rock_well/core.py
@@ -1,3 +1,4 @@
+from random import choice
 from .models import Input, Output, Result, Throw
 
 
@@ -18,18 +19,23 @@ RULES_FIRST_WIN = {
 def match(a: Throw, b: Throw) -> Result:
     # There are only three possible outcomes:
     #  1. Equal throws get draw (Result 1)
+    if a == b:
+        return Result.DRAW
     #  2. Different throws
     #    2.1 If they are in the winning rules it gets win (Result 2)
+    if (a, b) in RULES_FIRST_WIN:
+        return Result.WIN
     #    2.2 If they are not in the winning rules it gets loss (Result 3)
-    pass
+    return Result.LOSS
 
 
 # Get a random throw (the computer's throw) using the values of Throw
 def random_throw() -> Throw:
-    pass
+    return choice([v for v in Throw])
 
 
 # Given the user's throw, the function get the computer's throw
 # and return the result for the play.
 def play(input: Input) -> Output:
-    pass
+    throw = random_throw()
+    return Output(computer_throw=throw, result=match(input.throw, throw))
