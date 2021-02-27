0e93cf9e84a6f4ea67cf0b7e9a8a969dbfbb9d70
npujol <naivy.luna@gmail.com>
2021-02-27 11:38:55 +0100

    Description of the core

---
+++ scissors_paper_rock_well/core.py
@@ -0,0 +1,35 @@
+from .models import Input, Output, Result, Throw
+
+
+# The rules for the cases of win, when the first value beats the second one.
+# I included only the win's cases, because to know the loss's cases you only have
+# to change the order of the values.
+RULES_FIRST_WIN = {
+    # (user's throw, computer's throw)
+    (Throw.SCISSORS, Throw.PAPER),
+    (Throw.PAPER, Throw.ROCK),
+    (Throw.PAPER, Throw.WELL),
+    (Throw.ROCK, Throw.SCISSORS),
+    (Throw.WELL, Throw.ROCK),
+    (Throw.WELL, Throw.SCISSORS),
+}
+
+# With two throws(one from the user and other from the computer) return a result
+def match(a: Throw, b: Throw) -> Result:
+    # There are only three possible outcomes:
+    #  1. Equal throws get draw (Result 1)
+    #  2. Different throws
+    #    2.1 If they are in the winning rules it gets win (Result 2)
+    #    2.2 If they are not in the winning rules it gets loss (Result 3)
+    pass
+
+
+# Get a random throw (the computer's throw) using the values of Throw
+def random_throw() -> Throw:
+    pass
+
+
+# Given the user's throw, the function get the computer's throw
+# and return the result for the play.
+def play(input: Input) -> Output:
+    pass
