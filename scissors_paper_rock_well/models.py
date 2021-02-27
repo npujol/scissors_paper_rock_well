b001db9ccc4107eceb8102cb91127862ac1444aa
npujol <naivy.luna@gmail.com>
2021-02-27 10:25:12 +0100

    Add the models for Throw, Result, Input and Output

--- scissors_paper_rock_well/models.py
+++ scissors_paper_rock_well/models.py
@@ -1,18 +1,27 @@
+from enum import Enum
+from pydantic import BaseModel
+
 # The possible throw values (scissors, paper, rock, well)
-class Throw:
-    pass
+class Throw(Enum):
+    SCISSORS = "scissors"
+    ROCK = "rock"
+    PAPER = "paper"
+    WELL = "well"
 
 
 # The possible result values (win, loss, draw)
-class Result:
-    pass
+class Result(Enum):
+    WIN = "win"
+    LOSS = "loss"
+    DRAW = "draw"
 
 
 # A Throw object(user entry)
-class Input:
-    pass
+class Input(BaseModel):
+    throw: Throw
 
 
 # A Throw object(the computer throw) and a Result
-class Output:
-    pass
+class Output(BaseModel):
+    computer_throw: Throw
+    result: Result
