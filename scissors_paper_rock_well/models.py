8a43dfb133d11b4606984bcbb5e9d93e5937b939
npujol <naivy.luna@gmail.com>
2021-02-27 10:04:36 +0100

    Decription of the models

---
+++ scissors_paper_rock_well/models.py
@@ -0,0 +1,18 @@
+# The possible throw values (scissors, paper, rock, well)
+class Throw:
+    pass
+
+
+# The possible result values (win, loss, draw)
+class Result:
+    pass
+
+
+# A Throw object(user entry)
+class Input:
+    pass
+
+
+# A Throw object(the computer throw) and a Result
+class Output:
+    pass
